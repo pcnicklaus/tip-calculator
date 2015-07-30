254add7887c0f22df9288ea4e5563d92f9544a79
Patrick <pcnicklaus@gmail.com>
2015-07-29 23:44:48 -0600

    Working on js. have logic down but am missing something. will work on again soon.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 // add scripts
 
+var answer = 0;
+
 $(document).on('ready', function() {
   console.log('sanity check!');
+  var myButton = document.getElementById(generateButton);
+  myButton.onclick = myFunction () {
+    console.log('firing');
+    var amount = document.getElementById(amount);
+    parseInt(amount);
+console.log(amount);
+    var tip = document.getElementById(tip);
+    parseInt(tip);
+    console.log(tip);
+    tipAmount = (amount * tip);
+    tipAmount.toFixed(2);
+    document.getElementById('answer').innerHTML = tipAmount;
+
+  };
+
 });
+
